a0f39081a481e2a09afbc8782eba5474564c4554
Fiona Fairbairn <fairbairnf@gmail.com>
2019-06-27 15:42:55 +0100

    format changes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const apiRouter = require("./routes/api-router.js");
 app.use(express.json());
 app.use("/api", apiRouter);
+app.use("/*", (req, res) => {
+  res.status(404).send({ msg: "Page not found." });
+});
 
 app.use((err, req, res, next) => {
   // console.log(err);
