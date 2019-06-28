3104c793a326ea5447acd76e4558407229b16ff3
Fiona Fairbairn <fairbairnf@gmail.com>
2019-06-28 13:07:31 +0100

    changes made for hosting production db

--- db/index.js
+++ db/index.js
@@ -2,5 +2,9 @@ const ENV = process.env.NODE_ENV || "development";
 const devData = require("./development-data/index");
 const testData = require("./test-data/index");
 
-if (ENV === process.env.NODE_ENV) module.exports = testData;
+if (ENV === "test") module.exports = testData;
+if (ENV === "production") module.exports = devData;
 if (ENV === "development") module.exports = devData;
+
+// const data = { test: testData, development: devData, production: devData };
+// module.exports = data[ENV]
